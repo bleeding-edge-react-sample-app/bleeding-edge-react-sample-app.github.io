8b782b7839bdc6efc11345234efa05e728300ba1
Frankie Bagnardi <f.bagnardi@gmail.com>
2015-09-15 01:49:09 -0700

    adds api util

---
+++ src/utils/api/api.js
@@ -0,0 +1,40 @@
+import {Promise} from 'bluebird';
+import superagent from 'superagent';
+import superagentPromise from 'superagent-promise';
+export const agent = superagentPromise(superagent, Promise);
+const API_ROOT = '/api';
+
+// api token
+const TOKEN_KEY = 'authorizationToken';
+let token = localStorage.getItem(TOKEN_KEY) || null;
+
+// request wrapper
+export function makeRequest(method, url){
+  var req = agent[method.toLowerCase()](
+    `${API_ROOT}/${url}`
+  );
+
+  if (token) {
+    req.set('Authorization', `JWT ${token}`);
+  }
+
+  return req;
+}
+export default {
+  get: (...args) => makeRequest('get', ...args),
+  post: (...args) => makeRequest('post', ...args),
+  put: (...args) => makeRequest('put', ...args),
+  delete: (...args) => makeRequest('delete', ...args),
+  head: (...args) => makeRequest('head', ...args),
+};
+
+// manages api token
+export function setToken(newToken){
+  localStorage.setItem(TOKEN_KEY, newToken);
+  console.debug('Api Token: %s', newToken);
+  token = newToken;
+}
+
+export function getToken(){
+  return token;
+}
