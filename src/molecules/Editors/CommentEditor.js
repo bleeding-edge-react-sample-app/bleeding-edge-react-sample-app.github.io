eabb3babb72c636069089de21e866bf6bf1ba535
Frankie Bagnardi <f.bagnardi@gmail.com>
2015-09-19 05:07:41 -0700

    adds comment editor

---
+++ src/molecules/Editors/CommentEditor.js
@@ -0,0 +1,28 @@
+import React, {PropTypes} from 'react';
+import Box from '../../atoms/Box';
+import TextInput from '../../atoms/TextInput';
+
+export default
+class CommentEditor extends React.Component {
+  static propTypes = {
+    value: PropTypes.any,
+    onChange: PropTypes.func,
+  };
+
+  render(){
+    return (
+      <Box grow="1">
+        <TextInput
+          label="Your comment"
+          tag="textarea"
+          direction="column"
+          value={this.props.value && this.props.value.text || ''}
+          onChange={(e) => {
+            this.props.onChange({text: e.target.value, type: 'comment'})
+          }}
+          style={{flexGrow: '1'}}
+        />
+      </Box>
+    );
+  }
+}
