d728ef6cc15e33849cc6fb66c9c4878a23d04019
Frankie Bagnardi <f.bagnardi@gmail.com>
2015-11-02 01:27:51 -0700

    Revert "trying animations on MainLayout"
    This reverts commit 1ddbfca3fb2e4651825944d2f472278fe10691cc.

--- src/pages/MainLayout/MainLayout.js
+++ src/pages/MainLayout/MainLayout.js
@@ -3,17 +3,14 @@ import Box from '../../atoms/Box';
 import SubBoardHeaderWrapper from '../../organisms/SubBoardHeaderWrapper';
 import EditingPaneWrapper from '../../organisms/EditingPaneWrapper';
 import MainNavigation from '../../molecules/MainNavigation';
-import {TransitionMotion, spring} from 'react-motion';
 
 export default
 class MainLayout extends React.Component {
+  static propTypes = {
 
-  static contextTypes = {
-    history: PropTypes.any,
   };
 
   render(){
-    window.h = this.context.history;
     return (
       <Box>
         <Box>
@@ -21,50 +18,12 @@ class MainLayout extends React.Component {
           <MainNavigation />
         </Box>
         <Box margin="1em">
-          {this.renderContent()}
+          {this.props.children}
         </Box>
         <Box>
           <EditingPaneWrapper />
         </Box>
       </Box>
-    );
-  }
-  renderContent(){
-    var enterStyles = {
-      left: spring(-105),
-    };
-    var leaveStyles = {
-      left: spring(105),
-    };
-
-    // better way to get the location?
-    var key = window.location.href;
-
-    var styles = {
-      [key]: {
-        left: spring(0)
-      }
-    };
-
-    return (
-      <TransitionMotion
-        willEnter={() => enterStyles}
-        willLeave={() => leaveStyles}
-        styles={styles}
-        children={(allStyles) => {
-          var styles = allStyles[key];
-          return (
-            <Box
-              children={this.props.children}
-              style={{
-                left: styles.left + '%',
-                position: 'absolute',
-              }}
-            />
-          );
-        }}
-      />
-
     );
   }
 }
