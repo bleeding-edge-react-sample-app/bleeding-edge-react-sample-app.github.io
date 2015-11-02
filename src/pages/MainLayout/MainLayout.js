1ddbfca3fb2e4651825944d2f472278fe10691cc
Frankie Bagnardi <f.bagnardi@gmail.com>
2015-11-02 01:27:35 -0700

    trying animations on MainLayout

--- src/pages/MainLayout/MainLayout.js
+++ src/pages/MainLayout/MainLayout.js
@@ -3,14 +3,17 @@ import Box from '../../atoms/Box';
 import SubBoardHeaderWrapper from '../../organisms/SubBoardHeaderWrapper';
 import EditingPaneWrapper from '../../organisms/EditingPaneWrapper';
 import MainNavigation from '../../molecules/MainNavigation';
+import {TransitionMotion, spring} from 'react-motion';
 
 export default
 class MainLayout extends React.Component {
-  static propTypes = {
 
+  static contextTypes = {
+    history: PropTypes.any,
   };
 
   render(){
+    window.h = this.context.history;
     return (
       <Box>
         <Box>
@@ -18,12 +21,50 @@ class MainLayout extends React.Component {
           <MainNavigation />
         </Box>
         <Box margin="1em">
-          {this.props.children}
+          {this.renderContent()}
         </Box>
         <Box>
           <EditingPaneWrapper />
         </Box>
       </Box>
+    );
+  }
+  renderContent(){
+    var enterStyles = {
+      left: spring(-105),
+    };
+    var leaveStyles = {
+      left: spring(105),
+    };
+
+    // better way to get the location?
+    var key = window.location.href;
+
+    var styles = {
+      [key]: {
+        left: spring(0)
+      }
+    };
+
+    return (
+      <TransitionMotion
+        willEnter={() => enterStyles}
+        willLeave={() => leaveStyles}
+        styles={styles}
+        children={(allStyles) => {
+          var styles = allStyles[key];
+          return (
+            <Box
+              children={this.props.children}
+              style={{
+                left: styles.left + '%',
+                position: 'absolute',
+              }}
+            />
+          );
+        }}
+      />
+
     );
   }
 }
